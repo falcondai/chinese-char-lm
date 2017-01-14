502b1757a7429ebca3e932753010add745c6a6f5
Falcon Dai <me@falcondai.com>
2017-01-14 15:48:00 -0600

    improve glyph render APIs

--- render.py
+++ render.py
@@ -5,12 +5,13 @@ from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
 def render(text, font):
-    return font.getmask(text)
-
-def print_glyph_mask(mask):
+    mask = font.getmask(text)
     size = mask.size[::-1]
     a = np.asarray(mask).reshape(size)
-    for l in a:
+    return a
+
+def ascii_print(glyph_array):
+    for l in glyph_array:
         for c in l:
             if c != 0:
                 print '#',
@@ -19,8 +20,10 @@ def print_glyph_mask(mask):
         print
 
 if __name__ == '__main__':
+    s = u'你好'
     print 'check utf-8 support:',
-    print '你好'
+    print s.encode('utf-8')
     font = ImageFont.truetype('NotoSansCJKsc-hinted/NotoSansCJKsc-Regular.otf', 24)
-    m = font.getmask(u'你好')
-    print_glyph_mask(m)
+    a = render(s, font)
+    print a
+    ascii_print(a)
