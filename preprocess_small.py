14058b4701affdac5ab1fd04fd9bd4018ab3f2fa
Jon <jonczbean@gmail.com>
2017-04-01 21:08:09 -0500

    testing

--- preprocess_small.py
+++ preprocess_small.py
@@ -22,7 +22,8 @@ def char_split(file_handle):
 
             for char in word:
                 new_line.append(char.encode('utf-8'))
-            seg_index.append(len(new_line))
+            seg_index + [0]*(len(word)-1)
+            seg_index.append(1)
         yield new_line, seg_index
         continue
     # return raw_lines, seg_indices
@@ -50,23 +51,23 @@ if __name__ == '__main__':
             fraw_handle.close()
             fseg_handle.close()
 
-    for path in train_paths:
-        file_name = path.split('/')[-1].split('.')[0].split('_')[0]
-        print file_name
-        file_handle_dict = {'train': (open('segmentation/'+file_name+'_train_raw', 'wb'), 
-                                    open('segmentation/'+file_name+'_train_seg', 'wb')),
-                            'val': (open('segmentation/'+file_name+'_val_raw', 'wb'), 
-                                    open('segmentation/'+file_name+'_val_seg', 'wb'))}
+    # for path in train_paths:
+    #     file_name = path.split('/')[-1].split('.')[0].split('_')[0]
+    #     print file_name
+    #     file_handle_dict = {'train': (open('segmentation/'+file_name+'_train_raw', 'wb'), 
+    #                                 open('segmentation/'+file_name+'_train_seg', 'wb')),
+    #                         'val': (open('segmentation/'+file_name+'_val_raw', 'wb'), 
+    #                                 open('segmentation/'+file_name+'_val_seg', 'wb'))}
 
-        with open(path, 'rb') as fread_handle:
+    #     with open(path, 'rb') as fread_handle:
 
 
-            for raw_line, seg_index in char_split(fread_handle):
-                fhandle_tuple = file_handle_dict[np.random.choice(['train', 'val'], p=[0.9, 0.1])]
-                fhandle_tuple[0].write(' '.join(raw_line))
-                fhandle_tuple[0].write('\n')
-                fhandle_tuple[1].write(' '.join([str(index) for index in seg_index]))
-                fhandle_tuple[1].write('\n')
+    #         for raw_line, seg_index in char_split(fread_handle):
+    #             fhandle_tuple = file_handle_dict[np.random.choice(['train', 'val'], p=[0.9, 0.1])]
+    #             fhandle_tuple[0].write(' '.join(raw_line))
+    #             fhandle_tuple[0].write('\n')
+    #             fhandle_tuple[1].write(' '.join([str(index) for index in seg_index]))
+    #             fhandle_tuple[1].write('\n')
 
 
 
