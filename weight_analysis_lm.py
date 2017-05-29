e6f7643169332bb713715d18bec0076455d25f0c
Jonbean <jonczbean@gmail.com>
2017-05-29 00:10:28 -0500

    weight analysis first trial

--- weight_analysis_lm.py
+++ weight_analysis_lm.py
@@ -1,110 +1,129 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import numpy as np
+import cv2
 import tensorflow as tf
+from tensorflow.contrib.tensorboard.plugins import projector
 # from tensorflow.python import debug as tfdbg
 import os, glob
+from train_id_cnn_lm import generate_glyphs, render_glyph
 
-from train_id_cnn_lm import build_input_pipeline, build_model, generate_glyphs
+def rebuild_model(token_ids, glyphs, seq_lens, vocab_size, embed_dim, n_cnn_layers, n_cnn_filters, wemb):
+    # encoder
+    bs = tf.size(seq_lens)
+    wemb = wemb
+    glyph_unaware = tf.nn.embedding_lookup(wemb, token_ids)
 
-def test(test_split_path, dict_path, log_dir, batch_size, vocab_size, n_oov_buckets, print_interval):
-    # token to token-id lookup
-    vocabulary = tf.contrib.lookup.string_to_index_table_from_file(dict_path, num_oov_buckets=n_oov_buckets, vocab_size=vocab_size)
+    # glyph-aware
+    net = glyphs / 255.
+    net = tf.reshape(net, (-1, 24, 24, 1))
+    net = tf.contrib.layers.convolution2d(
+        inputs=net,
+        num_outputs=32,
+        kernel_size=(7, 7),
+        stride=(2, 2),
+        activation_fn=tf.nn.relu,
+        biases_initializer=tf.zeros_initializer(),
+        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+        scope='conv1',
+    )
 
-    # input pipelines
-    # test
-    ids, seq_lens, lines = build_input_pipeline(test_split_path, vocabulary, batch_size, shuffle=False, allow_smaller_final_batch=True, num_epochs=1)
-    seq_lens -= 1
+    # net = tf.contrib.layers.convolution2d(
+    #     inputs=net,
+    #     num_outputs=16,
+    #     kernel_size=(5, 5),
+    #     stride=(2, 2),
+    #     activation_fn=tf.nn.relu,
+    #     biases_initializer=tf.zeros_initializer(),
+    #     weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+    #     scope='conv2',
+    # )
+
+    net = tf.contrib.layers.flatten(net)
+    net = tf.contrib.layers.fully_connected(
+        inputs=net,
+        num_outputs=embed_dim,
+        biases_initializer=tf.zeros_initializer(),
+        weights_initializer=tf.contrib.layers.xavier_initializer(),
+        activation_fn=None,
+        scope='embedding_fc',
+    )
+
+    glyph_aware = tf.reshape(net, (bs, -1, embed_dim))
+
+    return glyph_unglyph_aware
+
+def compute_embeddings(checkpoint_dir, dict_path, vocab_size, n_oov_buckets, embed_dim):
+    glyph_width = 24
+
+    token_id_ph = tf.placeholder('int32', shape=[None], name='center_token_id')
 
     # model
     glyph_ph = tf.placeholder('float', shape=[None, None, 24, 24], name='glyph')
+    # token to token-id lookup
+
+    seq_lens = tf.stack([tf.shape(token_ids_ph)[0]])
+
+    id_token_vocabulary = tf.contrib.lookup.index_to_string_table_from_file(dict_path, vocab_size=vocab_size)
+
     embed_dim, rnn_dim = 100, 64
     n_cnn_layers, n_cnn_filters = 0, 32
+    saver = tf.train.Saver()
+    seq_lens = [1]
     with tf.variable_scope('model'):
-        seq_logits, final_state, id_emb, cnn_emb = build_model(ids[:, :-1], glyph_ph[:, :-1], seq_lens, vocab_size + n_oov_buckets, embed_dim, rnn_dim, n_cnn_layers, n_cnn_filters)
+        id_emb, glyph_emb = rebuild_model(token_ids = token_id_ph, glyphs = glyph_ph, seq_lens = seq_lens, vocab_size = vocab_size, embed_dim = embed_dim, n_cnn_layers = n_cnn_layers, n_cnn_filters = n_cnn_filters)
 
-    # loss
-    mask = tf.sequence_mask(seq_lens, dtype=tf.float32)
-    loss = tf.contrib.seq2seq.sequence_loss(seq_logits, ids[:, 1:], mask, average_across_timesteps=True, average_across_batch=True)
-    n_samples = tf.reduce_sum(mask)
+    # if not os.path.exists(embeddings_checkpoint_dir):
+    #     os.makedirs(embeddings_checkpoint_dir)
 
-    id_accumulative_vec = np.zeros(embed_dim)
-    cnn_accumulative_vec = np.zeros(embed_dim)
+    # # visualize embeddings
+    # projector_config = projector.ProjectorConfig()
+    # embed = projector_config.embeddings.add()
+    # embed.tensor_name = embeddings.name
+    # metadata_path = os.path.join(embeddings_checkpoint_dir, 'projector_metadata.txt')
+    # embed.metadata_path = metadata_path 
+    # Load the VGG-16 model in the default graph
+    latest_checkpoint_path = tf.train.latest_checkpoint(checkpoint_dir)
+    print '* restoring model from checkpoint %s' % latest_checkpoint_path
+    saver.restore(sess, latest_checkpoint_path)
 
+    saver = tf.train.import_meta_graph(checkpoint_dir)
+    # Access the graph
+    graph = tf.get_default_graph()
+
+    # Retrieve variables
+    conv_bias = graph.get_tensor_by_name('model/conv1/biases:0')
+    conv_weight = graph.get_tensor_by_name('model/conv1/weights:0')
+    wemb_fc_weight = graph.get_tensor_by_name('model/embedding_fc/weights:0')
+    wemb_fc_bias = graph.get_tensor_by_name('model/embedding_fc/biases:0')
+    wemb_matrix = graph.get_tensor_by_name('model/EmbedSequence/embeddings:0')
+    
     config = tf.ConfigProto(gpu_options={'allow_growth': True})
     with tf.Session(config=config) as sess:
-        # to debug
-        # sess = tfdbg.LocalCLIDebugWrapperSession(sess)
 
-        checkpoint_dir = os.path.join(log_dir, 'checkpoints')
-        saver = tf.train.Saver(tf.global_variables())
-        latest_checkpoint_path = tf.train.latest_checkpoint(checkpoint_dir)
-        print '* restoring model from checkpoint %s' % latest_checkpoint_path
-        saver.restore(sess, latest_checkpoint_path)
+        id_token_dict = id_token_vocabulary.eval()
 
-        # initialize the token-id lookup table and local variables
-        # before starting the queue runners
-        sess.run([tf.local_variables_initializer(), tf.tables_initializer()])
+        embeddings_val = np.zeros((vocab_size + n_oov_buckets, embed_dim))
+        for i in range(vocab_size):
+            token = sess.run([])
+            feed_dictionary = {token_ids_ph:i, glyph_ph:render_glyph(id_token_dict[i].decode('utf8'), shape=(24, 24))}
 
-        # check uninitialized variables
-        uninitialized_variables = tf.report_uninitialized_variables().eval()
-        if len(uninitialized_variables) > 0:
-            print '* some variables are not restored:'
-            for v in uninitialized_variables:
-                print v
-            return None
-
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-
-        total_loss = 0.
-        total_n = 0
-        i = 0
-        try:
-            while not coord.should_stop():
-                ids_val, seq_lens_val, lines_val = sess.run([ids, seq_lens, lines])
-
-                # generate glyphs for characters
-                glyphs = generate_glyphs(ids_val, lines_val)
-
-                n, loss_val, id_emb, cnn_emb = sess.run([n_samples, loss, id_input, cnn_input], {
-                    ids: ids_val,
-                    seq_lens: seq_lens_val,
-                    glyph_ph: glyphs,
-                })
-
-
-                id_accumulative_vec += np.sum(np.sum(id_emb, axis = 1), axis = 0)
-                cnn_accumulative_vec += np.sum(np.sum(cnn_emb, axis = 1), axis = 0)
-
-                total_loss += n * loss_val
-                total_n += n
-                if i % print_interval == 0:
-                    print 'minibatch %i loss %g' % (i, loss_val)
-                i += 1
-            coord.join(threads)
-        except tf.errors.OutOfRangeError:
-            print 'epoch limit reached'
-        except Exception as e:
-            coord.request_stop(e)
-
-    per_symbol_loss = total_loss / total_n
-    print '* %i samples test loss %g perplexity %g' % (total_n, per_symbol_loss, np.exp(per_symbol_loss))
-    return per_symbol_loss
+            sess.run([id_emb, glyph_emb], feed_dict=feed_dictionary)
+            print id_emb, glyph_emb
+        # embed_saver.save(sess, os.path.join(embeddings_checkpoint_dir, 'embeddings'))
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log-dir', required=True)
+
+    parser.add_argument('-l', '--import-log-dir', required=True, help='path to skip-gram model checkpoint directory')
     parser.add_argument('-b', '--batch-size', type=int, default=16)
-    parser.add_argument('-m', '--vocab-size', type=int, default=1000)
+    parser.add_argument('-m', '--vocab-size', type=int, default=4000)
     parser.add_argument('-o', '--n-oov-buckets', type=int, default=1)
-    parser.add_argument('-t', '--test-corpus', default='work/test.txt', help='path to the test split')
+    parser.add_argument('--embed-dim', type=int, default=100)
     parser.add_argument('--dictionary', default='work/dict.txt', help='path to the dictionary file')
-    parser.add_argument('--print-interval', type=int, default=16, help='interval of printing minibatch evaluation results')
 
     args = parser.parse_args()
 
-    test(args.test_corpus, args.dictionary, args.log_dir, args.batch_size, args.vocab_size, args.n_oov_buckets, args.print_interval)
+    compute_embeddings(args.import_log_dir, args.dictionary, args.vocab_size, args.n_oov_buckets, args.embed_dim)
