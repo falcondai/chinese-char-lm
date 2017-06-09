dfdd3a25de3b4f4d7ed2018be6642561bd98b168
Jonbean <jonczbean@gmail.com>
2017-06-09 14:00:44 -0500

    building vocabulary for individual task

--- vocabulary_builder.py
+++ vocabulary_builder.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding:utf-8-*-
-import operator
+import operator, sys
 
 class Vocabulary_Builder(object):
     """docstring for Vocabulary_Builder"""
     def __init__(self, vocabulary_dict = {}):
         super(Vocabulary_Builder, self).__init__()
         self.vocabulary_dict = vocabulary_dict
-        self.save_vocabulary_path = './work/vocabulary_frequency_dictionary.txt'
-
+        self.start_tag = '<P>'
+        self.end_tag = '</P>'
 
     def read_in(self, file_path):
         # 1. read through the file
         print "reading..."
 
-        with open(file_path,'r') as f:
+        with open(file_path, 'rb') as f:
             doc = f.readlines()
             for line in doc:
                 chars = line.split()
@@ -27,22 +27,32 @@ class Vocabulary_Builder(object):
         print "vocabulary building complete"
 
         # 2. sorting according to frequency
-        sorted_dict = sorted(self.vocabulary_dict.items(), key=operator.itemgetter(1), reverse=True)
-
+        # put special symbols in the front
+        self.vocabulary_dict[self.start_tag] = sys.maxint
+        self.vocabulary_dict[self.end_tag] = sys.maxint
+        self.sorted_dict = sorted(self.vocabulary_dict.items(), key=operator.itemgetter(1), reverse=True)
 
         # 3. saving dictionary to a txt file
+    def save(self, saving_path):
         print "saving..."
-        with open(self.save_vocabulary_path, 'w') as f:
-            for items in sorted_dict:
-                f.write(items[0] + '\t' + str(items[1]))
+        with open(saving_path, 'wb') as f:
+            for char, freq in self.sorted_dict:
+                f.write(char)
                 f.write('\n')
         print "done"
 
 if __name__ == '__main__':
-    train_set_path = './work/train.txt'
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--corpus-path', required=True)
+    parser.add_argument('-d', '--dictionary-path')
+    args = parser.parse_args()
+
+    train_set_path = args.corpus_path
+    saving_path = args.dictionary_path
 
     vocab_builder = Vocabulary_Builder()
-    
+
     vocab_builder.read_in(train_set_path)
-
-
+    vocab_builder.save(saving_path)
