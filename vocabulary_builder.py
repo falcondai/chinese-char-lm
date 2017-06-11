b990d615ec53e5759f5e4b5d9491ae5c8fa003a3
Jonbean <jonczbean@gmail.com>
2017-06-10 19:34:12 -0500

    checkout from preprocess

--- vocabulary_builder.py
+++ vocabulary_builder.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding:utf-8-*-
-import operator, sys
+import operator
 
 class Vocabulary_Builder(object):
     """docstring for Vocabulary_Builder"""
     def __init__(self, vocabulary_dict = {}):
         super(Vocabulary_Builder, self).__init__()
         self.vocabulary_dict = vocabulary_dict
-        self.save_vocabulary_path = './work/dict.txt'
-        self.start_tag = '<P>'
-        self.end_tag = '</P>'
+        self.save_vocabulary_path = './work/vocabulary_frequency_dictionary.txt'
+
 
     def read_in(self, file_path):
         # 1. read through the file
         print "reading..."
 
-        with open(file_path, 'rb') as f:
+        with open(file_path,'r') as f:
             doc = f.readlines()
             for line in doc:
                 chars = line.split()
@@ -28,28 +27,22 @@ class Vocabulary_Builder(object):
         print "vocabulary building complete"
 
         # 2. sorting according to frequency
-        # put special symbols in the front
-        self.vocabulary_dict[self.start_tag] = sys.maxint
-        self.vocabulary_dict[self.end_tag] = sys.maxint
         sorted_dict = sorted(self.vocabulary_dict.items(), key=operator.itemgetter(1), reverse=True)
+
 
         # 3. saving dictionary to a txt file
         print "saving..."
-        with open(self.save_vocabulary_path, 'wb') as f:
-            for char, freq in sorted_dict:
-                f.write(char)
+        with open(self.save_vocabulary_path, 'w') as f:
+            for items in sorted_dict:
+                f.write(items[0] + '\t' + str(items[1]))
                 f.write('\n')
         print "done"
 
 if __name__ == '__main__':
-    # import argparse
-    #
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-c', '--corpus-path', required=True)
-    # parser.add_argument('-d', '--dictionary-path')
-
-    train_set_path = sys.argv[1]
+    train_set_path = './work/train.txt'
 
     vocab_builder = Vocabulary_Builder()
-
+    
     vocab_builder.read_in(train_set_path)
+
+
